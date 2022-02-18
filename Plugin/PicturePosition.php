ee2762c76a537b136d6aa02d9a44ff56731e143f
Alex <magefast@gmail.com>
2022-02-18 23:59:30 +0200

    module

---
+++ Plugin/PicturePosition.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @author magefast@gmail.com www.magefast.com
+ */
+
+namespace Strekoza\FixPicturePosition\Plugin;
+
+use Magento\Catalog\Block\Adminhtml\Product\Helper\Form\Gallery;
+
+/**
+ *
+ */
+class PicturePosition
+{
+    /**
+     * @param Gallery $subject
+     * @param $return
+     * @return mixed
+     */
+    public function afterGetImages(Gallery $subject, $return)
+    {
+        $checkPosition = [];
+        foreach ($return['images'] as $key => $value) {
+            $position = intval($value['position']);
+            while (isset($checkPosition[$position])) {
+                $position = $position + 1;
+            }
+
+            $return['images'][$key]['position'] = $position;
+            $checkPosition[$position] = $position;
+        }
+
+        return $return;
+    }
+}
